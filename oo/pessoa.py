087fca6bf7931e129b159485d8076bfce2faf056
Arthur <tavares.arthur@ymail.com>
2021-04-03 16:39:26 -0300

    Criado a classe Pessoa Criado o método cumprimentar Criado atributos deinstância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,15 @@
 class Pessoa:
+    def __init__(self,nome = None, idade = 3):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Bernardo')
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Arthur'
+    print(p.nome)
+    print(p.idade)
